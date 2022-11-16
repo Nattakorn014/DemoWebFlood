00e059fc8b34ae830230a9c52dc983072dc79073
owenxdp <phoominphosuwan2692000@gmail.com>
2022-11-17 00:59:31 +0700

    testttt

--- DemoWebFlood/Models/ErrorViewModel.cs
+++ DemoWebFlood/Models/ErrorViewModel.cs
@@ -3,7 +3,7 @@ namespace DemoWebFlood.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-
+        /*test*/
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
