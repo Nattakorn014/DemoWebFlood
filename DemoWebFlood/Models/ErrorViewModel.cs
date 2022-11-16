48d808c7c9961b54789cf16f7b2389787ec98232
Nattakorn014 <62011212014@msu.ac.th>
2022-11-16 15:11:03 +0700

    Add project files.

---
+++ DemoWebFlood/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoWebFlood.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
